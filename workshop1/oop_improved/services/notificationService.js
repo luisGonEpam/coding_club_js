7770d5d5de3a1ebcbc1f2850997afcdba19ccb4f
ldgonzalez26 <ldgonzalezmedina@gmail.com>
2024-10-29 14:05:56 -0500

    removed oop_improved
    Signed-off-by: ldgonzalez26 <ldgonzalezmedina@gmail.com>

--- workshop1/oop_improved/services/notificationService.js
+++
@@ -1,7 +0,0 @@
-class NotificationService {
-  sendNotification(email, message) {
-    console.log(`Sending notification to ${email}: ${message}`);
-  }
-}
-
-module.exports = NotificationService;
--- workshop1/oop_improved/services/smsService.js
+++
@@ -1,9 +0,0 @@
-const NotificationService = require('./notificationService');
-
-class SMSService extends NotificationService {
-  sendNotification(phoneNumber, message) {
-    console.log(`Sending SMS to ${phoneNumber}: ${message}`);
-  }
-}
-
-module.exports = SMSService;
